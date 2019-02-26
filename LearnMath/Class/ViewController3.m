868746ea28338443580f3152b624a9e45014d910
ABChe <abche777@163.com>
2019-02-26 22:35:45 +0800

    class3:迭代法
    求方程的精确或者近似解

--- LearnMath/Class/ViewController3.m
+++ LearnMath/Class/ViewController3.m
@@ -18,7 +18,8 @@
     [super viewDidLoad];
     self.questionDescription = @"在你曾经的项目中是否用过迭代法？如果有，你觉得迭代法最大的特点是什么？如果没用过，你想想看现在的项目中是否有可以使用的地方？";
     
-    [self example1];
+//    [self example1];
+    [self example2];
 }
 
 
@@ -53,5 +54,73 @@
     return sum;
 }
 
+
+#pragma mark -
+
+
+/**
+ 求方程的精确或者近似解
+ */
+- (void)example2 {
+    /*
+     给定一个大于 1 的正整数，求它的平方根
+     */
+    NSInteger number = 10.f;
+    CGFloat deltaThreshould = 0.000001f;
+    NSInteger maxtry = 100;
+    
+    CGFloat squreRoot = [self getSqureRootWithNumber:number deltaThreshould:deltaThreshould maxTry:maxtry];
+    
+    NSString *string;
+    if (squreRoot == -1.f) {
+        string = @"数值应大于1";
+    } else if (squreRoot == -2.f) {
+        string = @"未找到解";
+    } else {
+        string = [NSString stringWithFormat:@"%lf", squreRoot];
+    }
+    
+    NSLog(@"正整数 %ld 的平方根为 %@", number, string);
+}
+
+
+/**
+ 计算大于 1 的正整数的平方根
+
+ @param number 大于一的正整数
+ @param deltaThreshould 误差的阈值
+ @param maxTry 二分查找的最大次数
+ */
+- (CGFloat)getSqureRootWithNumber:(NSInteger)number deltaThreshould:(CGFloat)deltaThreshould maxTry:(NSInteger)maxTry {
+    
+    if (number <= 1) return -1.f;
+    
+    CGFloat min = 1.f;
+    CGFloat max = (CGFloat)number;
+    
+    for (int i = 0; i < maxTry; i++) {
+        CGFloat middle = (min + max) / 2;
+        CGFloat delta = fabs(pow(middle, 2) / number - 1);
+        
+        if (delta <=deltaThreshould) {
+            // 满足精度要求
+            return middle;
+        }
+        else {
+            if (pow(middle, 2) > number) {
+                // 左半边
+                max = middle;
+            } else {
+                // 右半边
+                min = middle;
+            }
+        }
+        
+//        if (i == maxTry - 1) {
+//            return middle;
+//        }
+    }
+    return -2.f;
+}
 
 @end
