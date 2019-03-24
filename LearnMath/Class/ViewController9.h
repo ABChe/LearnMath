d3e33dd6d1c4345b70fa1111f57be1881d6a2291
ABChe <abche777@163.com>
2019-03-24 21:25:31 +0800

    class9:动态规划（上）

---
+++ LearnMath/Class/ViewController9.h
@@ -0,0 +1,27 @@
+//
+//  ViewController9.h
+//  LearnMath
+//
+//  Created by 车 on 2019/3/24.
+//  Copyright © 2019 车. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ViewController9 : BaseViewController
+
+/*
+ 1. 什么是动态规划?
+ 通过不断分解问题，可以将复杂的任离简化为最基本的小问题。我们需要在各种可能的局部解中。找出可能达到最优的局部解。这个寻找最优解的过程就是动态规划。
+ 
+ 2.动态规划的关键是什么?
+ 动态规划特别注重子同题之间的转移关系。我们把子问题之间的转移称为状态转移，并把用干刻画这些状态转移的表达式称为状态转移方程。找到合适的状态转格方程，是动态规划的关键。
+ 
+ 3.由一个字符串转成另一个字符串所常的最少编辑操作次数。我们叫作编辑距离。编辑距离越小，这两个学符串越相似。
+ */
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LearnMath/Class/ViewController9.m
@@ -0,0 +1,24 @@
+//
+//  ViewController9.m
+//  LearnMath
+//
+//  动态规划
+//  Created by 车 on 2019/3/24.
+//  Copyright © 2019 车. All rights reserved.
+//
+
+#import "ViewController9.h"
+
+@interface ViewController9 ()
+
+@end
+
+@implementation ViewController9
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    self.questionDescription = @"理解了动态规划法和状态转移之后，你觉得根据编辑l距离来衡量字符串之间的相似程度有什么局限性？你有什么优化方案吗？";
+}
+
+@end
