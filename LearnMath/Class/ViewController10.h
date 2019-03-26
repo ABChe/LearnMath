66d5461b492e528b5f6dbf843081c27710165363
ABChe <abche777@163.com>
2019-03-26 22:14:03 +0800

    class10:动态规划（下）

---
+++ LearnMath/Class/ViewController10.h
@@ -0,0 +1,26 @@
+//
+//  ViewController10.h
+//  LearnMath
+//
+//  动态规划
+//  Created by 车 on 2019/3/26.
+//  Copyright © 2019 车. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ViewController10 : BaseViewController
+
+
+/*
+ 1.状态转移方程是什么?
+ 从上一个状态到下一个状态之间可能存在的一些变化。以及基于这些变化的最终决策结果。我们把这样的表达式称为状态转移方程。我上节最开始就说过。在所有动态规划的解法中。状态转移方程是关键。所以你一定要掌探它。
+ 
+ 2基于编辑距需的算法有什么局限性?
+ 基干编辑距离的算法也有易限性，它只适用拉丁语系的相似度衡量，所以通常只用于英文或者拼音相关的查询。如果在中文这种亚洲语系中，差一个汉字(或字符)语义就会差很远。所以并不适合使用基于编辑距离的算法。
+ */
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LearnMath/Class/ViewController10.m
@@ -0,0 +1,22 @@
+//
+//  ViewController10.m
+//  LearnMath
+//
+//  Created by 车 on 2019/3/26.
+//  Copyright © 2019 车. All rights reserved.
+//
+
+#import "ViewController10.h"
+
+@interface ViewController10 ()
+
+@end
+
+@implementation ViewController10
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+@end
